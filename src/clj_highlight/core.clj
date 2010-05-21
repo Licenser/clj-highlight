ecadb95e5435425b59e8a06947aa98bd2c74bbae
Heinz N. Gies <heinz@licenser.net>
2010-05-21 03:30:59 +0200

    Testing to work on performance.

--- src/clj_highlight/core.clj
+++ src/clj_highlight/core.clj
@@ -2,30 +2,39 @@
   (:use clj-highlight.mangler)
   (:import java.util.Scanner))
 
-(defn- token-seq* [s token-def defs states]
-   (lazy-seq 
-    (if (empty? defs)
-      (conj (token-seq* (subs s 1) token-def (get token-def (first states)) states) [:error (str (first s))  {:state states}])
-      (let [[matcher & defs] defs] 
-	(if (empty? s)
-	  '()
-	  (if-let [result (matcher s states token-def)]
-	    (let [[token states token-def defs] result]
-	      (conj (token-seq* (subs s (count (fnext token))) token-def defs states) token))
-	    (token-seq* s token-def defs states)))))))
 
-(defn tokenizer [syntax]
+(defn- next-token [s idx token-def defs states]
+  (if (empty? defs)
+    [[:error (str (first s))  {:state states}] states token-def (get token-def (first states))]
+    (let [[matcher & defs] defs] 
+      (if-let [result (matcher s idx states token-def)]
+	result
+	(recur s idx token-def defs states)))))
+
+(defn- token-seq* [s idx size token-def defs states] 
+  (lazy-seq
+   (if (= idx size)
+     '()
+     (let [[token states token-def defs] (next-token s idx token-def defs states)]
+       (cons token (token-seq* s (+ idx (count (fnext token))) size token-def defs states))))))
+
+(defn tokenizer 
+  "Creates a tokenizer for a given syntax definition."
+  [syntax]
   (let [tkn
     	(fn tokenizer*
 	  ([string state]
-	     (token-seq* string syntax (get syntax state) (list state)))
+	     (token-seq* string 0 (count string) syntax (get syntax state) (list state)))
 	  ([string]
 	  (tokenizer* string :initial)))]
     (if-let [keywords (:keywords syntax)]
       (mangle-tokens :identifier (fn [k t s] (if (keywords t) [:keyword t s] [k t s])) tkn)
       tkn)))
 
-(defn highlighter [syntax output & manglers]
+(defn highlighter
+  "Creates a highlighter consisting of a given syntax, a output generator and a set of manglers.
+The manglers are applied to the token stream in the order before it is passed to the output generator."
+  [syntax output & manglers]
   (let [tkn (reduce (fn [tkn mgl] (mgl tkn)) (tokenizer syntax) manglers)]
     (fn highlighter* [code]
       (output (tkn code)))))
--- src/clj_highlight/syntax/general.clj
+++ src/clj_highlight/syntax/general.clj
@@ -5,22 +5,25 @@
 (defn token [matcher kind & [new-state info-fn]]
   (cond
    (nil? new-state)
-   (fn [string states token-def]
-     (if-let [token (matcher string)]
+   (fn [string idx states token-def]
+     (if-let [token (matcher string idx)]
        [[kind token (if info-fn (info-fn kind token states) {:state states})] states token-def ((first states) token-def)]))
    (= new-state :pop)
-   (fn [string states token-def]
-     (if-let [token (matcher string)]
+   (fn [string idx states token-def]
+     (if-let [token (matcher string idx)]
        (let [states (next states)]
 	 [[kind token (if info-fn (info-fn kind token states) {:state states})] states token-def ((first states) token-def)])))
    :else
-   (fn [string states token-def]
-     (if-let [token (matcher string)]
+   (fn [string idx states token-def]
+     (if-let [token (matcher string idx)]
        (let [states (conj new-state states)]
 	 [[kind token (if info-fn (info-fn kind token states) {:state states})] states token-def ((first states) token-def)])))))
 
+
 (defn re-token [re kind & [new-state]]
-  (token 
-   (partial re-find (re-pattern (str "^(?:" re ")")))
-   kind
-   new-state))
+  (let [pattern (re-pattern (str "^(?:" re ")"))]
+    (token 
+     (fn [string idx]
+       (re-find pattern (subs string idx)))
+     kind
+     new-state)))
